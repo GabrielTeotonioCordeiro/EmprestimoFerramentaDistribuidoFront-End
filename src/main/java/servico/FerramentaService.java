478d9d12081b6122f6b4ef837da9653d0e007113
Periusdv <dvperius@gmail.com>
2025-11-10 15:37:07 -0300

    build: Criando classe FerramentaService.

---
+++ src/main/java/servico/FerramentaService.java
@@ -0,0 +1,17 @@
+package servico;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class FerramentaService {
+    private static IFerramenta instanciaFerramenta;
+   
+    public static IFerramenta getInstanciaFerramenta() throws Exception{
+        if (instanciaFerramenta == null) {
+            Registry registro = LocateRegistry.getRegistry("localhost");
+            Object obj = registro.lookup("FerramentaService");
+            instanciaFerramenta = (IFerramenta) obj;
+        }
+        return null;
+    }
+}
